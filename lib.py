ee1d9132691b4051f254dfeaf85eca2c4b753302
David Cottrell <cottrell@users.noreply.github.com>
2017-09-08 23:49:54 +0100

    update

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import lib
+import pickle
 import json
 import os
 import urllib
@@ -8,39 +9,53 @@ import mylib.tools
 
 _mydir = os.path.dirname(os.path.realpath(__file__))
 
-n_max = 3
+n_max = -1
 data_dir = os.path.join(_mydir, 'data')
 
 def max_limit(gen):
-    for i, x in enumerate(gen):
-        if i >= n_max:
-            break
-        yield x
+    if n_max < 1:
+        for x in gen:
+            yield x
+    else:
+        for i, x in enumerate(gen):
+            if i >= n_max:
+                break
+            yield x
 
 class CC():
-    def ls(self):
+    def list_crawls(self):
         out, err, status = self._ls()
         out = [x.strip().rstrip('/') for x in out if '-MAIN-' in x]
         out = [re.sub('^PRE ', '', x) for x in out]
         return out
     def _ls(self):
         return lib_cached.run_command_get_output('aws s3 ls s3://commoncrawl/crawl-data/')
-    def get_paths(self, name):
-        dirname = os.path.join(data_dir, name)
+    def get_paths(self, crawl):
+        dirname = os.path.join(data_dir, crawl)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
+        res = dict()
         for k in ['warc', 'wet', 'wat']:
             filename = os.path.join(dirname, '{}.paths.gz'.format(k))
             if os.path.exists(filename):
                 print('filename exists, skipping: {}'.format(filename))
             else:
-                cmd = 'cd {}  && ( curl -O https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2017-04/{}.paths.gz ; cd - ; )'.format(dirname, k)
+                cmd = 'cd {}  && ( curl -O https://commoncrawl.s3.amazonaws.com/crawl-data/{}/{}.paths.gz ; cd - ; )'
+                cmd = cmd.format(crawl, dirname, k)
                 print('running {}'.format(cmd))
                 mylib.tools.run_command_get_output(cmd)
-    def get_paths_all(self):
-        out = self.ls()
+            res
+    def get_paths_all_crawls(self):
+        out = self.list_crawls()
         for x in max_limit(out):
             self.get_paths(x)
+    def search_all_crawls(self, text):
+        """ search across all known crawls """
+        out = self.list_crawls()
+        res = list()
+        for crawl in max_limit(out):
+            res.extend(self.search(crawl, text))
+        return res
     def search(self, crawl, text):
         r = self._search(crawl, text)
         r = r.content.strip().split(b'\n')
@@ -48,10 +63,16 @@ class CC():
         return r
     def _search(self, crawl, text):
         text = urllib.parse.quote_plus(text)
+        filename = os.path.join(data_dir, crawl, text) + '.pickle'
+        if os.path.exists(filename):
+            print('reading {}'.format(filename))
+            return pickle.load(open(filename, 'rb'))
         url = 'http://index.commoncrawl.org/{}-index?url={}&output=json'.format(crawl, text)
-        return lib_cached.requests_get(url)
-
-
+        res = lib_cached.requests_get(url)
+        print('writing {}'.format(filename))
+        if not os.path.exists(os.path.dirname(filename)):
+            os.makedirs(os.path.dirname(filename))
+        pickle.dump(res, open(filename, 'wb'))
+        return res
 
 cc = CC()
-
