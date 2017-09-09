936a2a50190cb838aaeffb9b72c907449c1ad6a4
David Cottrell <cottrell@users.noreply.github.com>
2017-09-09 23:37:45 +0100

    asdf

--- lib.py
+++ lib.py
@@ -9,8 +9,10 @@ import mylib.tools
 
 _mydir = os.path.dirname(os.path.realpath(__file__))
 
-n_max = -1
-data_dir = os.path.join(_mydir, 'data')
+n_max = 3
+data_dir = os.path.join(_mydir, 'crawl-data') # some things will break if this is not called crawl-data
+
+_baseurl = "https://commoncrawl.s3.amazonaws.com"
 
 def max_limit(gen):
     if n_max < 1:
@@ -30,21 +32,19 @@ class CC():
         return out
     def _ls(self):
         return lib_cached.run_command_get_output('aws s3 ls s3://commoncrawl/crawl-data/')
-    def get_paths(self, crawl):
+    def get_paths(self, crawl, return_result=False):
         dirname = os.path.join(data_dir, crawl)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         res = dict()
         for k in ['warc', 'wet', 'wat']:
             filename = os.path.join(dirname, '{}.paths.gz'.format(k))
-            if os.path.exists(filename):
-                print('filename exists, skipping: {}'.format(filename))
-            else:
-                cmd = 'cd {}  && ( curl -O https://commoncrawl.s3.amazonaws.com/crawl-data/{}/{}.paths.gz ; cd - ; )'
-                cmd = cmd.format(crawl, dirname, k)
-                print('running {}'.format(cmd))
-                mylib.tools.run_command_get_output(cmd)
-            res
+            cmd = 'curl {baseurl}/crawl-data/{crawl}/{filetype}.paths.gz > {dirname}/{filetype}.paths.gz'
+            cmd = cmd.format(crawl=crawl, baseurl=_baseurl, dirname=dirname, filetype=k)
+            res[k] = lib_cached.run_cmd_with_cachefile(filename, cmd, return_result=return_result)
+        if return_result:
+            res = {k: v.decode().split('\n') for k, v in res.items()}
+            return res
     def get_paths_all_crawls(self):
         out = self.list_crawls()
         for x in max_limit(out):
@@ -57,22 +57,26 @@ class CC():
             res.extend(self.search(crawl, text))
         return res
     def search(self, crawl, text):
-        r = self._search(crawl, text)
-        r = r.content.strip().split(b'\n')
+        text = urllib.parse.quote_plus(text)
+        filename = os.path.join(data_dir, crawl, text) + '.json'
+        url = 'http://index.commoncrawl.org/{}-index?url={}&output=json'.format(crawl, text)
+        cmd = "curl -s '{}' > {}".format(url, filename)
+        r = lib_cached.run_cmd_with_cachefile(filename, cmd, return_result=True)
+        r = r.strip().split('\n')
         r = [json.loads(x) for x in r]
         return r
-    def _search(self, crawl, text):
-        text = urllib.parse.quote_plus(text)
-        filename = os.path.join(data_dir, crawl, text) + '.pickle'
-        if os.path.exists(filename):
-            print('reading {}'.format(filename))
-            return pickle.load(open(filename, 'rb'))
-        url = 'http://index.commoncrawl.org/{}-index?url={}&output=json'.format(crawl, text)
-        res = lib_cached.requests_get(url)
-        print('writing {}'.format(filename))
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-        pickle.dump(res, open(filename, 'wb'))
-        return res
+    def get_crawl_from_json(self, json_entry, return_result=True):
+        """
+        curl -H "range: bytes=822555329-822557378" -O https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2017-04/segments/1484560279657.18/warc/CC-MAIN-20170116095119-00156-ip-10-171-10-70.ec2.internal.warc.gz
+        """
+        start = int(json_entry['offset'])
+        stop = start + int(json_entry['length'])
+        filename = json_entry['filename']
+        cmd = "curl -H 'range: {start}-{stop}' -O '{baseurl}/{filename}' > {filename}"
+        cmd = cmd.format(start=start, stop=stop, baseurl=_baseurl, filename=filename)
+        dirname = os.path.dirname(filename)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        return lib_cached.run_cmd_with_cachefile(filename, cmd, return_result=return_result)
 
 cc = CC()
--- lib_cached.py
+++ lib_cached.py
@@ -1,7 +1,26 @@
-# put things in here to avoid reset cache on imp.reload
+"""
+Put things in here to avoid reset cache on imp.reload.
+
+"""
 import mylib.tools
 from functools import lru_cache
 import requests
+import gzip
+import os
 
 run_command_get_output = lru_cache()(mylib.tools.run_command_get_output)
 requests_get = lru_cache()(requests.get)
+
+# cached and persisted
+@lru_cache()
+def run_cmd_with_cachefile(filename, cmd, *args, return_result=True, **kwargs):
+    if not os.path.exists(filename):
+        print('running {}'.format(cmd))
+        run_command_get_output(cmd)
+        assert os.path.exists(filename), 'filename {} was not created!'.format(filename)
+    if return_result:
+        if filename.endswith('.gz'):
+            return gzip.open(filename).read()
+        else:
+            return open(filename).read()
+        
