2f7455c22023c07a8859340147ab05ce2261c250
David Cottrell <cottrell@users.noreply.github.com>
2017-09-10 11:14:41 +0100

    as mod

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='commoncrawlscraper',
+      version='0.0',
+      description='crawl small subsets with persistent caching',
+      packages=['commoncrawlscraper'],
+      zip_safe=False)
